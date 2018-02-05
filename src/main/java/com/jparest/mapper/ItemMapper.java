4410391e007d0b4d0307bb35ef401c6aba113283
bbarbs <barberobrainold@gmail.com>
2018-02-05 18:32:40 +0800

    Ignore unmappedTargetPolicy in mapper

--- src/main/java/com/jparest/mapper/ItemMapper.java
+++ src/main/java/com/jparest/mapper/ItemMapper.java
@@ -5,10 +5,14 @@ import com.jparest.model.dto.ItemDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(
+        componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface ItemMapper {
 
     @Mappings({
